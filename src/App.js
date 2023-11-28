74c88c1fea72816051c20b1d3c54ce2d36c7959b
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2023-11-28 11:34:48 +0600

    Date Counter-v2 project.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  return <Counter />;
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date("Nov 22 2023");
+  date.setDate(date.getDate() + count);
+
+  function countForward() {
+    return setCount((c) => c + step);
+  }
+  function countBackward() {
+    return setCount((c) => c - step);
+  }
+
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <div>
+        <div>
+          <input
+            type="range"
+            min={1}
+            max={10}
+            value={step}
+            onChange={(e) => setStep(+e.target.value)}
+          />
+          <span>Step {step}</span>
+        </div>
+        <div>
+          <button onClick={countBackward}>-</button>
+          <input
+            type="text"
+            value={count}
+            onChange={(e) => setCount(+e.target.value)}
+          />
+          <button onClick={countForward}>+</button>
+        </div>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()} </span>
+      </p>
+      {step !== 1 || count !== 0 ? (
+        <button onClick={handleReset}>Reset</button>
+      ) : null}
     </div>
   );
 }
